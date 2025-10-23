e45a2df80d2e5658f82b3e72508236e9d5ed82ed
deansure <caleb@deansure.com>
2025-10-23 09:07:16 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+// Grab the button and the nav
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
 
+// When the button is clicked, toggle the "active" class on the nav
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
